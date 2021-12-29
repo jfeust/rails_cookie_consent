8d1f5984dd3be4252834fe96aa319928ad1e8afa
Tomáš Král <kralutomas@gmail.com>
2021-12-29 12:30:11 +0100

    fix cookie consent parsing

--- lib/rails_cookie_consent/version.rb
+++ lib/rails_cookie_consent/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module RailsCookieConsent
-  VERSION = "0.2.0".freeze
+  VERSION = "0.2.1".freeze
   COOKIES_CONSENT_VERSION = '2.7.1'.freeze # https://github.com/orestbida/cookieconsent
 end
